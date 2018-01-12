74409749b955fe65f7be53b67e124635b1245469
Twesh <tweshc@gmail.com>
2018-01-11 20:47:56 -0500

    Added endpoint
    Added /getInstanceId endpoint to retreive endpoint id of eureka client service via RestTemplate.exchange()

--- src/main/java/com/example/demo/EurekaHelloApplication.java
+++ src/main/java/com/example/demo/EurekaHelloApplication.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -18,8 +22,22 @@ public class EurekaHelloApplication{
 
 @RestController
 class MyController{
+	
+	RestTemplate restTemplate = new RestTemplate();
+	
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "Hello";
     }
+    
+    @RequestMapping(value="/getInstanceId", method= RequestMethod.GET)
+    public String getInstanceId() {
+    	
+    	String url = "http://localhost:8888/instanceId";
+    	
+    	ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<String>(){} );
+    	System.out.println(response.getBody().toString());
+    	return response.getBody().toString();
+    }
+    
 }
